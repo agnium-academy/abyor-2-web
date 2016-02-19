d9f4bc18d877a745b190d4a13517f1c2b74b4d2d
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-19 11:52:53 +0700

    Complete logic for body mass index calc

--- js/calculate.js
+++ js/calculate.js
@@ -6,23 +6,36 @@ var height = document.formBmi.height.value
 var sex = document.formBmi.sex.value
 if(weight > 0 && height > 0){
 var resultBmi = weight/(height/100*height/100)
+// NHANES II Definitions form BMI
 document.formBmi.bmi.value = resultBmi
-if(resultBmi <= 17.0 && age > 0 && sex == male ){
-document.formBmi.result.value = "You are too thin."
+if(resultBmi =< 20.7 && age > 0 && sex == male ){
+document.formBmi.result.value = "You are Underweight."
 }
-else if(resultBmi <= 24.0 && age > 0 && sex == female ){
-document.formBmi.result.value = "You are too thin."
+else if(resultBmi <= 19.1 && age > 0 && sex == female ){
+document.formBmi.result.value = "You are Underweight."
 }
-if(resultBmi > 18.5 && resultBmi < 25 && age > 0 && sex == male){
+if(resultBmi > 20.7 && resultBmi =< 26.4 && age > 0 && sex == male){
 document.formBmi.result.value = "You are in normal range."
 }
-else if(resultBmi <= 24.0 && age > 0 && sex == female ){
+else if(resultBmi > 19.1 && resultBmi =< 25.8 && age > 0 && sex == female ){
 document.formBmi.result.value = "You are in normal range."
 }
-if(resultBmi > 25 && resultBmi < 30){
-document.formBmi.result.value = "You are overweight."
+if(resultBmi > 26.4 && resultBmi =< 27.8 && age > 0 && sex == male){
+document.formBmi.result.value = "You are Marginally Overweight."
 }
-if(resultBmi > 30){
+else if(resultBmi > 25.8 && resultBmi =< 27.3 && age > 0 && sex == female ){
+document.formBmi.result.value = "You are Marginally Overweight."
+}
+if(resultBmi > 27.8 && resultBmi =< 31.1 && age > 0 && sex == male){
+document.formBmi.result.value = "You are Overweight."
+}
+else if(resultBmi > 27.3 && resultBmi =< 32.3 && age > 0 && sex == female ){
+document.formBmi.result.value = "You are Overweight."
+}
+if(resultBmi > 31.1 && age == 0 && sex == male){
+document.formBmi.result.value = "You are obesity."
+}
+if(resultBmi > 32.3 && age == 0 && sex == female){
 document.formBmi.result.value = "You are obesity."
 }
 }
